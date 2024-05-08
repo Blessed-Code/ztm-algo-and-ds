5d2509077598e2d5feb01dfbc9f4cb8170636614
Bernardus Joshua Otto Prayoga <bernarduuuus@gmail.com>
2024-05-08 15:59:51 +0700

    add comment on bigOExercise2.sj

--- 00_bigO/bigOExercise2.js
+++ 00_bigO/bigOExercise2.js
@@ -3,7 +3,7 @@ function anotherFunChallenge(input) {
     let a = 5; // O(1)
     let b = 10; // O(1)
     let c = 50; // O(1)
-    for (let i = 0; i < input; i++) { // O(n)
+    for (let i = 0; i < input; i++) { // O(n) // ternyata ini bisa kagak di include
       let x = i + 1; // O(n)
       let y = i + 2; // O(n)
       let z = i + 3; // O(n)
@@ -15,3 +15,4 @@ function anotherFunChallenge(input) {
     let whoAmI = "I don't know"; // O(1)
   }
   // Big O = 4 + 7n = O(n)
+  // kalau loop tidak diinclude jadinya 4 + 5n = O(n)
