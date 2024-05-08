8ee5619f366da81925a0dd6e7cca32439c03bd61
Bernardus Joshua Otto Prayoga <bernarduuuus@gmail.com>
2024-05-08 18:22:25 +0700

    add compressBoxTwices.js and note.txt

---
+++ 00_bigO/compressBoxTwices.js
@@ -0,0 +1,15 @@
+// rule 3
+function compressBoxesTwices(boxes, boxes2) {
+    boxes.forEach(box => {
+        console.log(box);
+    });
+
+    boxes2.forEach(box => {
+        console.log(box);
+    });
+}
+
+// ini bukan O(n) karena loopingnya beda sumber
+// kalo forEachnya untuk boxes aja maka bener jadi O(n)
+// karena ini 2 sumber berbeda anggepannya kaya O(n + m)
+// terus gimana kalo nested array?
