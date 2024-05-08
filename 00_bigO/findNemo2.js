c0a43ff0b328a94cdf6d6e365827a1feebb08b34
Bernardus Joshua Otto Prayoga <bernarduuuus@gmail.com>
2024-05-08 15:03:15 +0700

    add findNemo2.js for checkin in milliseconds

---
+++ 00_bigO/findNemo2.js
@@ -0,0 +1,20 @@
+const nemo = ['nemo'];
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(1000).fill('nemo');
+
+function findNemo (array) {
+    // untuk test performance gak bagus cuma berdasar siapa yagn paling cepet. 
+    // ada banyak faktor, kaya cpunya, languagenya dll. ini untuk measurement awal aja
+    let t0 = performance.now();
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "nemo") {
+            console.log("Found NEMO!");
+        }
+    }
+    let t1 = performance.now();
+    console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
+
+findNemo(large);
